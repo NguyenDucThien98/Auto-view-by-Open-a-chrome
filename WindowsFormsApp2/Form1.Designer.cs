45d88552464e46c392b6828120cb0b0942606890
NguyenDucThien98 <NguyenDucThien98@users.noreply.github.com>
2019-11-07 23:33:31 +0700

    .

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApp2 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(402, 225);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(192, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(593, 221);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.name.Width = 500;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Status.Width = 90;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Delay :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 227);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "25";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 249);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Auto View - 0566662225";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KAutoHelper;
+namespace WindowsFormsApp2 {
+    public partial class Form1 : Form {
+        string[] files;
+        Process process = new Process();
+        Thread newThread;
+        public Form1() {
+            InitializeComponent();
+            button1.Enabled = false;
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+        }
+        void runCMD(string cmd) {
+            process.Start();
+            process.StandardInput.WriteLine(cmd);
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+        }
+
+        void getProfile(string url) {
+            files = Directory.GetFiles(url);
+            foreach (var item in files) {
+                dataGridView1.Rows.Add(item,"waiting");
+            }
+        }
+        bool isRunning = false;
+        void runChrome() {
+            if (isRunning) {
+                for (int i = 1; i <= 9999; i++) {
+                    if (isRunning) {
+                        for (int rows = 0; rows < dataGridView1.Rows.Count; rows++) {
+                            if (isRunning) {
+                                IPHostEntry ip;
+                                while (true) {
+                                    var check = false;
+                                    dataGridView1.Rows[rows].Cells[1].Value = "Change IP";
+                                    runCMD("rasdial viettel");
+                                    string myip = "";
+                                    ip = Dns.GetHostEntry(Dns.GetHostName());
+                                    foreach (IPAddress ipa in ip.AddressList) {
+                                        if (ipa.AddressFamily == AddressFamily.InterNetwork) {
+                                            myip = ipa.ToString();
+                                        }
+                                    }
+
+                                    if (File.Exists("ListIP.txt")) {
+                                        dataGridView1.Rows[rows].Cells[1].Value = "Check duplicate IP";
+                                        string[] listIP = File.ReadAllLines("ListIP.txt");
+                                        foreach (var item in listIP) {
+                                            var temp = item.Split(':');
+                                            if (myip == temp[0].Trim()) {
+                                                check = true;
+                                            }
+                                        }
+                                    }
+                                    if (!check) {
+                                        File.AppendAllText("ListIP.txt",myip + " : " + dataGridView1.Rows[rows].Cells[0].Value + "\n");
+                                        dataGridView1.Rows[rows].Cells[1].Value = "Open Chrome";
+                                        Process.Start(dataGridView1.Rows[rows].Cells[0].Value.ToString());
+                                        break;
+                                    } else {
+                                        runCMD("rasdial /disconnect");
+                                    }
+                                }
+                               
+                                if (isRunning) {
+                                    Thread.Sleep(2500);
+                                    var cap = CaptureHelper.CaptureScreen();
+                                    cap.Save("res.png");
+                                    var sub = ImageScanOpenCV.GetImage("icon.PNG");
+                                    var count = 0;
+                                    while (count <= 30) {
+                                        Point? res = ImageScanOpenCV.FindOutPoint((Bitmap) cap,sub);
+                                        if (res != null) {
+                                            AutoControl.MouseClick(res.Value.X,res.Value.Y,EMouseKey.LEFT);
+                                            Thread.Sleep(time * 1000);
+                                            break;
+                                        }
+                                        count++;
+                                    }
+                                }
+                                runCMD("taskkill /f /im chrome.exe");
+                                dataGridView1.Rows[rows].Cells[1].Value = "Disconnect Dcom";
+                                runCMD("rasdial /disconnect");
+                                if (isRunning) {
+                                    dataGridView1.Rows[rows].Cells[1].Value = "Done";
+                                } else {
+                                    dataGridView1.Rows[rows].Cells[1].Value = "Stop";
+                                }
+                            } else {
+                                dataGridView1.Rows[rows].Cells[1].Value = "Stop";
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+        int pID;
+        int time;
+        private void BtnStart_Click(object sender,EventArgs e) {
+            isRunning = true;
+            button1.Enabled = true;
+            btnStart.Enabled = false;
+            dataGridView1.Rows.Clear();
+            time = Int32.Parse(textBox1.Text);
+            using (var fbd = new FolderBrowserDialog()) {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) {
+                    getProfile(fbd.SelectedPath);
+                    newThread = new Thread(runChrome);
+                    newThread.Start();
+                    btnStart.Enabled = true;
+
+                } else {
+                    btnStart.Enabled = true;
+                    button1.Enabled = false;
+                }
+            }
+
+        }
+
+        private void Button1_Click(object sender,EventArgs e) {
+            isRunning = false;
+            btnStart.Enabled = true;
+            button1.Enabled = false;
+        }
+    }
+}
